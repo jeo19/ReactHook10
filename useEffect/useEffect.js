0816b96d3baa6d773bbcfccf88b492ec27d46246
jeo19 <terrakkk@outlook.com>
2020-04-13 23:14:55 +0800

    doc:If the second argument is no available, not display nothing on console log

---
+++ useEffect/useEffect.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+
+export default function App() {
+  const sayHello = () => console.log('Hello');
+
+  const [number, setNumber] = useState(0);
+  const [aNumber, setAnumber] = useState(0);
+  useEffect(sayHello, []);
+  return (
+    <div className="App">
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
+    </div>
+  );
+}

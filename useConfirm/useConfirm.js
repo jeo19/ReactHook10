bfd3235d7ce1b9dfcc150492dbef844308a5fab2
jeo19 <terrakkk@outlook.com>
2020-04-15 01:48:41 +0800

    feat:Implement the useConfirm hook

---
+++ useConfirm/useConfirm.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect, useRef } from 'react';
+const useConfirm = (message = '', onConfirm, onCancel) => {
+  if (!onConfirm || typeof onConfirm !== 'function') {
+    return;
+  }
+  if (!onCancel || typeof onCancel !== 'function') {
+    return;
+  }
+  const confirmAction = () => {
+    if (confirm(message)) {
+      onConfirm();
+    } else {
+      onCancel();
+    }
+  };
+  return confirmAction;
+};
+export default function App() {
+  const deleteWorld = () => {
+    console.log('Delete the world...');
+  };
+  const abort = () => {
+    console.log('Aborted');
+  };
+  const confirmDelete = useConfirm('Are you sure?', deleteWorld, abort);
+
+  return (
+    <div className="App">
+      <button onClick={confirmDelete}>Delete the world</button>
+    </div>
+  );
+}
